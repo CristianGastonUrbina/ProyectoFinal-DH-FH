a7f4f512c840adf5b21dedd837a69616fe537e64
German I. Paul <german@Mac-mini-de-German.local>
2021-12-08 23:09:21 -0300

    se agrega middleware para los usuarios no logueados

--- middlewares/usuarioNoLogueado.js
+++ middlewares/usuarioNoLogueado.js
@@ -2,21 +2,9 @@ const User = require("../entities/user");
 function usuarioNoLogueadoMiddleware(req, res, next) {
   let users = User.findAll();
   if (
-    req.session.usuariologueado == undefined &&
-    req.cookies.recuerdame != undefined
-  ) {
-    let usuarioALogearse;
-    for (let i = 0; i < users.length; i++) {
-      if (users[i].email === req.cookies.recuerdame) {
-        usuarioALogearse = users[i]; //Si lo encuentro, lo guardo
-        break;
-      }
-    }
-    if (usuarioALogearse == undefined) {
-        next();
-    }else{
-        break;
-    }
+    req.session.usuariologueado == undefined 
+  ) 
+  next();
 }
 
 module.exports = usuarioNoLogueadoMiddleware;
