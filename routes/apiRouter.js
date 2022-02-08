0390859c130fb9a8fdb35634115819b037507919
CristianGastonUrbina <ing.urbina.cristian@gmail.com>
2022-02-07 23:49:52 -0300

    API Users Done

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,8 +1,11 @@
 let express = require("express");
 let ApiProductController = require("../controllers/apis/productController");
-let ApiProductRouter = express.Router();
+let ApiRouter = express.Router();
+let ApiUserController = require("../controllers/apis/userController");
 
-ApiProductRouter.get("/Products", ApiProductController.list);
-ApiProductRouter.get("/Products/:id", ApiProductController.detail);
+ApiRouter.get("/Products", ApiProductController.list);
+ApiRouter.get("/Products/:id", ApiProductController.detail);
+ApiRouter.get("/Users", ApiUserController.list);
+ApiRouter.get("/Users/:id", ApiUserController.detail);
 
-module.exports = ApiProductRouter;
+module.exports = ApiRouter;
